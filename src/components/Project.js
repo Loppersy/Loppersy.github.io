53ace980f0e5f6f7bf5caf7780564474f5e0bf3d
Sebastian Lopez Figueroa <Loppersy7@gmail.com>
2024-09-05 19:40:18 -0700

    Fixed projects displaying in incorrect order in mobile screens. Increased mobile mode triggering width

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,33 +1,28 @@
 import React from "react";
 
 export function Project({ videoDemo, imageDemo, imgDemo, imageLocation, link1, link1Img, link2, link2Img, title, description, tags }) {
-    let image1
-    let image2
-    let link1Element
-    let link2Element
-    let demo
-    let titleLink // Variable for dynamically setting the title link
+    let demo;
+    let link1Element;
+    let link2Element;
+    let titleLink; // Variable for dynamically setting the title link
 
-    if(videoDemo !== "") {
-        demo = <video className={"demo"}  autoPlay={true} loop muted src={videoDemo}/>
+    // Determine the demo content (video or image)
+    if (videoDemo !== "") {
+        demo = <video className={"demo"} autoPlay={true} loop muted src={videoDemo} />;
     } else if (imageDemo !== "") {
-        demo = <img className={"demo"} src={imgDemo} alt={"Demo"}/>
-    }
-    if(imageLocation === "left") {
-        image1 = <div className={"demo-link"}>{demo}</div>;
-    } else {
-        image2 = <div className={"demo-link"}>{demo}</div>;
+        demo = <img className={"demo"} src={imgDemo} alt={"Demo"} />;
     }
 
-    if(link1 !== "" && link1Img !== ""){
-        link1Element = <a href={link1} target="_blank" rel="noreferrer"><img src={link1Img} alt={"Github"}/></a>
+    // Determine links and set the title link accordingly
+    if (link1 !== "" && link1Img !== "") {
+        link1Element = <a href={link1} target="_blank" rel="noreferrer"><img src={link1Img} alt={"Github"} /></a>;
         titleLink = link1; // Use link1 if available
     }
-    if(link2 !== "" && link2Img === ""){
-        link2Element =  <a href={`${link2}&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1`} target="_blank" rel="noreferrer"><img className={"googleImg"} alt='Get it on Google Play' src={'https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'}/></a>
+    if (link2 !== "" && link2Img === "") {
+        link2Element = <a href={`${link2}&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1`} target="_blank" rel="noreferrer"><img className={"googleImg"} alt='Get it on Google Play' src={'https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'} /></a>;
         if (!link1Element) titleLink = link2; // Use link2 if link1 is not set
     } else if (link2 !== "" && link2Img !== "") {
-        link2Element = <a href={link2} target="_blank" rel="noreferrer"><img src={link2Img} alt={"Link 2"}/></a>
+        link2Element = <a href={link2} target="_blank" rel="noreferrer"><img src={link2Img} alt={"Link 2"} /></a>;
         if (!link1Element) titleLink = link2; // Use link2 if link1 is not set
     }
 
@@ -38,9 +33,9 @@ export function Project({ videoDemo, imageDemo, imgDemo, imageLocation, link1, l
         <h2 className={"funny-title"}>{title}</h2> // Render without link if neither is available
     );
 
+    // Render the Project component
     return (
         <div className="project">
-            {image1}
             <div className="project-details">
                 {titleElement}
                 <p>{description}</p>
@@ -54,7 +49,9 @@ export function Project({ videoDemo, imageDemo, imgDemo, imageLocation, link1, l
                     {link2Element}
                 </div>
             </div>
-            {image2}
+            <div className="demo-link">
+                {demo}
+            </div>
         </div>
     );
 }
